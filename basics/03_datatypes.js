d3149f1e5f17a3379505a8019e196540f1c7c66d
Vishwajeet  kumar <122043037+vishwajeetkv@users.noreply.github.com>
2023-12-26 18:18:15 +0000

    datatypes

---
+++ basics/03_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code as newer version
+
+//alert = (3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+
+
+    console.log("vishwajeet")
+
+    let name = "vishwajeet"
+    let age = 19
+    let isloggedIn = false
+    let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+console.log(typeof undefined)//undefined
+console.log(typeof null )  //object
