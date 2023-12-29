d0490b3b069d6ace8dfc9155e8e6b49fd6c39907
Vishwajeet  kumar <122043037+vishwajeetkv@users.noreply.github.com>
2023-12-29 19:01:59 +0000

    Datatypes & memory

--- basics/06_DataTypes-summary.js
+++ basics/06_DataTypes-summary.js
@@ -1,3 +1,67 @@
 //Primitive
 
-//7 Types : 
+//7 Types : string ,Number ,Boolean, null, undefined, Symbol, BigInt
+
+//number
+const Score = 100
+const ScoreValue = 100.5
+
+//console.log(ScoreValue)
+
+//Boolean
+const IsLoggedIn = false
+
+//null
+const outsidetemp = null
+
+//undefined
+let userId;
+
+const id = Symbol ('123')
+const anotherId = Symbol('123')
+
+
+console.log(id === anotherId);
+
+
+// const bigNumber = 3456543576654356754n
+
+
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+
+//Stack (primitive), Heap (Non-Primitive)
+
+let myYoutubename = "vkverma@gmail.com"
+
+let anothername =  myYoutubename
+
+anothername = "chaiaurcode"
+
+console.log(myYoutubename)
+console.log(anothername);
+
+
+
+
+
+
+
+
