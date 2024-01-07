e3584b9977cd054ec84f01d938785ece92778518
Vishwajeet  kumar <122043037+vishwajeetkv@users.noreply.github.com>
2024-01-08 01:44:08 +0530

    Update 02_arrays

--- basics_02/02_arrays.js
+++ basics_02/02_arrays.js
@@ -1,7 +1,7 @@
 const marvel_heroes = ["thor" , "spiderman", "iroman"]
 const dc_heroes = ["superman" ,"flash" , "batman"]
 
-// marvel_heroes.push(dc_heroes)
+ marvel_heroes.push(dc_heroes)
 
 // console.log(marvel_heroes);
 // console.log(marvel_heroes [3][1]);
