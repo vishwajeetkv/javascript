eb83fc2e5cfd4927572fc7c9816973a2570aead4
Vishwajeet  kumar <122043037+vishwajeetkv@users.noreply.github.com>
2024-01-22 18:42:38 +0000

    iife

---
+++ basics_03/04_iife.js
@@ -0,0 +1,29 @@
+// Immediatly Invoked function Expression (IIFE)
+
+
+(function chai (){
+// named IIFE
+console.log(`DB CONNECTED`);
+
+})();
+
+
+( () => {
+
+console.log(`DB CONNECTED TWO ${name}`);
+
+})('vishwajeet')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
