063f9d892f4247b252aa53067a549c6c37a80672
Vishwajeet  kumar <122043037+vishwajeetkv@users.noreply.github.com>
2024-01-22 18:50:08 +0000

    iifee

--- basics_03/04_iife.js
+++ basics_03/04_iife.js
@@ -8,11 +8,21 @@ console.log(`DB CONNECTED`);
 })();
 
 
-( () => {
+((name) => {
 
 console.log(`DB CONNECTED TWO ${name}`);
-
 })('vishwajeet')
+
+
+// (function chai(){
+//     // named IIFE
+//     console.log(`DB CONNECTED`);
+// })();
+
+// ( (name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// } )('hitesh')
+
 
 
 
