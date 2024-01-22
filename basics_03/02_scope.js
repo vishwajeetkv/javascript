33ca141d75535678c1239932a58316e78c29d9ae
Vishwajeet  kumar <122043037+vishwajeetkv@users.noreply.github.com>
2024-01-22 17:00:21 +0000

    scope

---
+++ basics_03/02_scope.js
@@ -0,0 +1,67 @@
+
+
+let a = 30;
+const b = 40;
+var c = 45;
+
+if(true){
+    let a = 10;
+    const b =50;
+    // console.log("INNER: ", a);
+
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one (){
+    const username = "vishwajeet"
+   
+   
+    function two(){ 
+const website = "youtube"
+// console.log(username);
+
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+if (true){
+    const username = "vishwajeet"
+    if(username == "vishwajeet"){
+    const website = "ytube"
+    // console.log(username + website);
+
+}
+
+// console.log(website);
+}
+
+// console.log(username);
+
+
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addtwo(6)
+
+const addtwo = function (){
+    return num + 2
+}
+
+
+
+
+
+
+
+
