0abd0a12813ca712fa823060a80bfd32c304df68
Vishwajeet  kumar <122043037+vishwajeetkv@users.noreply.github.com>
2024-01-23 18:35:03 +0000

    one

---
+++ .../one.js
@@ -0,0 +1,51 @@
+// if 
+
+const isUserloggedin = true
+const temperature = 41
+
+
+// if ( temperature === 41 ){
+//     console.log(" less than 50 ");
+// }
+
+// else{
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("temperature is greater than 50");
+
+// console.log("execute");
+
+// const score = 200
+
+// if (score > 100 ) {
+// const power = "fly"
+// console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`); global scope (var=power)
+
+
+// const balance = 1000
+// if (balance > 500) console.log("test"),console.log("test2");
+if (balance < 500){    
+    
+    console.log("less than 500");
+
+}
+
+else if (balance < 950) {
+    console.log("less than 750")
+        
+    
+}
+else  {
+    console.log("less than 1200")
+        
+
+}
+
+
+
+
+
