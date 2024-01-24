6a2f344371ed677146ebea2f55fd40db2ecac437
Vishwajeet  kumar <122043037+vishwajeetkv@users.noreply.github.com>
2024-01-24 19:50:55 +0000

    switch

---
+++ control_flow_04/switch.js
@@ -0,0 +1,79 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 12
+
+switch (month) {
+    case 1:
+        console.log("january");
+        
+        break;
+
+    case 2:
+        console.log("feb");
+        
+        break;
+
+    case 3:
+        console.log("march");
+        
+        break;
+
+    case 4:
+        console.log("April");
+        
+        break;
+
+    case 5:
+        console.log("may");
+        
+        break;
+
+    case 6:
+        console.log("june");
+        
+        break;
+
+    case 7:
+        console.log("july");
+        
+        break;
+
+    case 8:
+        console.log("Aug");
+        
+        break;
+
+    case 9:
+        console.log("sep");
+        
+        break;
+
+    case 10:
+        console.log("oct");
+        
+        break;
+
+    case 11:
+        console.log("nov");
+        
+        break;
+
+    case 12:
+        console.log("Dec");
+        
+        break;
+
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
