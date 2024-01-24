affcaeb24c46b709bae9a87202388c2082c2baef
Vishwajeet  kumar <122043037+vishwajeetkv@users.noreply.github.com>
2024-01-24 19:51:56 +0000

    truthy.js

---
+++ control_flow_04/truthy.js
@@ -0,0 +1,52 @@
+const userEmail = []
+
+if (userEmail){
+    console.log("Got user email");
+
+}else{
+    console.log("dont have user email");
+}
+
+//  Falsy values
+
+// false , 0, -0, BigInt On ,"" , undefined, NaN
+
+// truthy values
+// "0" , 'false' , " ", [] , {} , function (){}
+
+
+if (userEmail.length === 0){
+    console.log("Array is empty");
+
+}
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+}
+
+// Nullish coalescing Operator (??): null undefined
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// var1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+console.log(val1);
+
+// Terniary Operator
+// consition ? true : false
+const iceTeaPrice = 100 
+iceTeaPrice <= 80 ? console.log("less than 80"): console.log(("more than 80"));
+
+
+
+
+
+
+
+
+
+ 
