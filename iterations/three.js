641fa4bcbaf7485d7e4f5c9ffc89d933b4b0513c
Vishwajeet  kumar <122043037+vishwajeetkv@users.noreply.github.com>
2024-01-26 19:08:14 +0000

    loop discussion

--- iterations/three.js
+++ iterations/three.js
@@ -1,26 +1,26 @@
-// for of
+// // for of
 
-// ["" , "", ""]
-// [{}, {} , {}]
+// // ["" , "", ""]
+// // [{}, {} , {}]
 
-const arr = [1,2,3,4,5]
+// const arr = [1,2,3,4,5]
 
-for (const num of arr){
-    // console.log(num);
-}
+// for (const num of arr){
+//     // console.log(num);
+// }
 
-const greetings = "Hello world"
-for(const greet of greetings){
-    // console.log(`Each char is ${greet}`);
-}
+// const greetings = "Hello world"
+// for(const greet of greetings){
+//     // console.log(`Each char is ${greet}`);
+// }
 
-// Maps
+// // Maps
 
-const map = new Map()
-map.set('IN' , "India")
-map.set('USA ', 'United States of America')
-map.set('IN' ,  "India")
-map.set('Fr ', 'France')
+// const map = new Map()
+// map.set('IN' , "India")
+// map.set('USA ', 'United States of America')
+// map.set('IN' ,  "India")
+// map.set('Fr ', 'France')
 
 // console.log(map);
 
@@ -40,3 +40,4 @@ map.set('Fr ', 'France')
 //     console.log(key , ':-' , value);
     
 // }
+
