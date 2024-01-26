1a264770771ba60f4ef1105d345350de88c6ce25
Vishwajeet  kumar <122043037+vishwajeetkv@users.noreply.github.com>
2024-01-26 19:07:26 +0000

    loop discussion

---
+++ iterations/four.js
@@ -0,0 +1,25 @@
+const myObject = {
+    js: "javascript",
+    cpp: "c++",
+    rb:"ruby",
+    swift: "swift by"
+
+}
+
+
+for (const key in myObject) {
+    
+        // console.log(myObject[key]);
+        // console.log(`${key} shortcut is for ${myObject[key]} `);
+    }
+const programming = ["js", "rb" , "py" ,"java" , "cpp"]
+
+for (const key in programming) {
+// console.log(key);
+console.log(programming[key]);
+}
+
+
+
+
+
