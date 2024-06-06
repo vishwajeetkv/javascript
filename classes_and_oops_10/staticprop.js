1f3e4196bd50a7386cb0516205b778fcc406910c
Vishwajeetverma <122043037+vishwajeetkv@users.noreply.github.com>
2024-06-06 10:25:36 +0000

    bind keyword in js

---
+++ classes_and_oops_10/staticprop.js
@@ -0,0 +1,29 @@
+class User {
+    constructor(username){
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+
+  static createId(){
+    return`123`
+}
+}
+
+const vishwajeet = new User("vishwajeet")
+// console.log(vishwajeet.createId())
+
+
+
+class Teacher extends User {
+    constructor ( username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher ("iphone, :i@phone.com")
+
+console.log(iphone.createId);
